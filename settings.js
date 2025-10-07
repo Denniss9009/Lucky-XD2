3005aa0a67f06197a7591c31ad234d19a999461c
Denniss9009 <osondudennis30@gmail.com>
2025-10-07 22:02:40 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ function convertToBool(text, trueValue = 'true') {
 
 module.exports = {
   // ===== BOT CORE SETTINGS =====
-  SESSION_ID: settings.SESSION_ID || process.env.SESSION_ID || "",
+  SESSION_ID: settings.SESSION_ID || process.env.SESSION_ID || "starcore~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiWUFLUnExQ0lZRjVvbDU5QkRJenJtV2l4SWdTMnJJME9yeTZIUzIrR0psOD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiV0FvWkxFM0dMQmM5QmFlcHgwUGZHSmtpcTQrZjlWS3dQR0F1VGdHcEhCYz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJhTVlSVW4yM0hHSEt0QjRiQ1VvY3JaNXFtQXFwYTRvWkUvZE9jZXN0YjFzPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJMN2xTYU1Xd1k2K3RYdktjQi96d2VNcXJFL05ETVN4cFVxdTBXbnpKZnhZPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImdNVkdldS9YSUtuSTF4a3k0WGNJU1BIN0RrTmt1NTlQVjlsc2ZvMVJSbGs9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ilg2QkxMSndVb1ZTeUQ0Y2hiU0lQcVdTQXczS1Q5L3JmZkxocDQvc2k1ejQ9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiU0ZPSnluSTdUUUpmcFpvTzB0NGhBL2JjeWhURFhaQnlNUjF1UG5PM0MxMD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiWktSZyt0ZDlSTk5nMFZOeGJaZUVjU3RxL05Rd3U0ZlNJZGNMSVRDblVVUT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlRuT1JvSFFmT2dTWkgyakFhdUJmaThHbGc2VmFYTE5hWUtBUUpuNzluTEF2YTYwSEY5RkhVc2JjYXFBYytvQUtNam1aM0FlcFlKcENIRjVUb3dxYkJ3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTMsImFkdlNlY3JldEtleSI6Ikk5ajN0K1ZGR3ZwM00wNjZMN3ZDamgwaDR1NGhuQUF1UlF0VzVWYlFDQmM9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6IkJDZjlzWGJfU09xTnVUMUNrM01zUEEiLCJwaG9uZUlkIjoiNWIzNWY0OTctMzZlNi00NTkwLTgyOWYtY2JjYTY4NzQ4MmY5IiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InoralQ3d2JXcVhXajVaNnJVREZCcWQrMU5KUT0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJIM0pnTXgzSGhyN2ZXWlQvY1NZS1VSR3JMbGc9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiTVJNQUxWSU4iLCJtZSI6eyJpZCI6IjIzNDgxNjUwMDEwODk6NUBzLndoYXRzYXBwLm5ldCIsImxpZCI6IjI3Njk0MDA3ODQ1Njg2Njo1QGxpZCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDUEw1dDl3RUVJUDNsY2NHR0FJZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiMlV1ZEh3S2ljZ1htRkpUN2lhYzRZMFI0N1U3cHBxWDFneXozZlRkZXAyWT0iLCJhY2NvdW50U2lnbmF0dXJlIjoiaVJUNVUvb0c5K0RGeGZGeDgwbWtiL014U3owTlJEWGt3aXBpQ3lvV2tlaCsyTzFqSVRhRG8wYjhrenE1Uk9BY01hM0FUMUI4UFhqNGU1VEo0NlU2QWc9PSIsImRldmljZVNpZ25hdHVyZSI6IkFOZVNDSlhaK3BzN2x2K2JqRVcyNGJ0SStsdjlBZjB6aFdpbnBObXRSRWtoTUlZYXJTUjdFWGhBRXlIY2ZjOVl2aDZRLzAxajh5S3dKMWNqM3FLQkJ3PT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjM0ODE2NTAwMTA4OTo1QHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQmRsTG5SOENvbklGNWhTVSs0bW5PR05FZU8xTzZhYWw5WU1zOTMwM1hxZG0ifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJyb3V0aW5nSW5mbyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkNBVUlEUT09In0sImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTc1OTg2OTg0MSwibXlBcHBTdGF0ZUtleUlkIjoiQUFBQUFIeHoifQ==",
   PREFIX: getConfig("PREFIX") || "." || settings.PREFIX,
   CHATBOT: getConfig("CHATBOT") || "on",
   BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "ʟᴜᴄᴋʏ-xᴅ",
@@ -24,8 +24,8 @@ module.exports = {
   BAILEYS: process.env.BAILEYS || "@whiskeysockets/baileys",
 
   // ===== OWNER & DEVELOPER SETTINGS =====
-  OWNER_NUMBER: settings.OWNER_NUMBER || process.env.OWNER_NUMBER || "256789966218",
-  OWNER_NAME: process.env.OWNER_NAME || getConfig("OWNER_NAME") || "ʟᴜᴄᴋʏ ➋➊➑",
+  OWNER_NUMBER: settings.OWNER_NUMBER || process.env.OWNER_NUMBER || "2348165001089",
+  OWNER_NAME: process.env.OWNER_NAME || getConfig("OWNER_NAME") || "LEGEND",
   DEV: process.env.DEV || "256789966218",
   DEVELOPER_NUMBER: '256789966218@s.whatsapp.net',
   MENU_AUDIO_URL: process.env.MENU_AUDIO_URL || 'https://files.catbox.moe/3v5i11.mp3',
@@ -46,7 +46,7 @@ NEWSLETTER_JID: process.env.NEWSLETTER_JID || '120363420656466131@newsletter',
   CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
   CUSTOM_REACT_EMOJIS: getConfig("CUSTOM_REACT_EMOJIS") || process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
   STICKER_NAME: process.env.STICKER_NAME || "ᴋʜᴀɴ-ᴍᴅ",
-  AUTO_STICKER: process.env.AUTO_STICKER || "false",
+  AUTO_STICKER: process.env.AUTO_STICKER || "true",
 
   // ===== MEDIA & AUTOMATION =====
   AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
@@ -57,10 +57,10 @@ NEWSLETTER_JID: process.env.NEWSLETTER_JID || '120363420656466131@newsletter',
   // ===== SECURITY & ANTI-FEATURES =====
   ANTI_DELETE: process.env.ANTI_DELETE || "true",
   ANTI_CALL: process.env.ANTI_CALL || "false",
-  ANTI_BAD_WORD: process.env.ANTI_BAD_WORD || "false",
+  ANTI_BAD_WORD: process.env.ANTI_BAD_WORD || "true",
   ANTI_LINK: process.env.ANTI_LINK || "true",
   ANTI_VV: process.env.ANTI_VV || "true",
-  DELETE_LINKS: process.env.DELETE_LINKS || "false",
+  DELETE_LINKS: process.env.DELETE_LINKS || "true",
   ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox",
   ANTI_BOT: process.env.ANTI_BOT || "true",
   PM_BLOCKER: process.env.PM_BLOCKER || "true",
@@ -72,9 +72,9 @@ NEWSLETTER_JID: process.env.NEWSLETTER_JID || '120363420656466131@newsletter',
   AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
   AUTO_BIO: process.env.AUTO_BIO || "false",
-  WELCOME: process.env.WELCOME || "false",
-  GOODBYE: process.env.GOODBYE || "false",
-  ADMIN_ACTION: process.env.ADMIN_ACTION || "false",
+  WELCOME: process.env.WELCOME || "true",
+  GOODBYE: process.env.GOODBYE || "true",
+  ADMIN_ACTION: process.env.ADMIN_ACTION || "true",
   version: process.env.version || "1.5.0",
   TIMEZONE: settings.TIMEZONE || process.env.TIMEZONE || "Africa/Kampala",
 
